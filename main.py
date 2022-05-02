986331a25d43f3e1168157428d15ff2aff3b6138
Alexandra <94623116+Alexandra2027@users.noreply.github.com>
2022-05-02 19:36:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
             print('Текст:', event.obj.message['text'])
             vk = vk_session.get_api()
             vk.messages.send(user_id=event.obj.message['from_id'],
-                             message=f"Дорый день, какой жанр вы желаете посмореть?",
+                             message=f"Дорый день, какой жанр вы желаете посмореть? {films_duration(0, 2, 3)}",
                              keybord='',
                              random_id=random.randint(0, 2 ** 64))
 
