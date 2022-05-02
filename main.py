a4b56995ea701e18a24bf1c0a0a13c66e173b2f7
Alexandra <94623116+Alexandra2027@users.noreply.github.com>
2022-05-02 19:06:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
+from mod import films_duration, films_year, films_genre
 TOKEN = 'fa08b59a045c6c6f861933963d3a2aadf7efbd01bf3292d7a08b7405afd2d3e658796555ea5f641b4e3e4'
 
 
 def main():
     vk_session = vk_api.VkApi(
         token=TOKEN)
-
     longpoll = VkBotLongPoll(vk_session, 211179909)
-
     for event in longpoll.listen():
-
         if event.type == VkBotEventType.MESSAGE_NEW:
             print(event)
             print('Новое сообщение:')
@@ -19,7 +17,8 @@ def main():
             print('Текст:', event.obj.message['text'])
             vk = vk_session.get_api()
             vk.messages.send(user_id=event.obj.message['from_id'],
-                             message="Спасибо, что написали нам. Мы обязательно ответим",
+                             message=f"Дорый день, какой жанр вы желаете посмореть?",
+                             keybord='',
                              random_id=random.randint(0, 2 ** 64))
 
 
